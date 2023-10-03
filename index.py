0d93ad59dd4651335e76410997cdcf18a109ac12
RickSanchezzz666 <lapiyktube@gmail.com>
2023-10-03 17:40:55 +0300

    pussy pussy boi

--- index.py
+++ index.py
@@ -1,104 +1,123 @@
-import time
-import random
+#-----------------------------------------------------------------------------------------
+#                                     Builder
+#-----------------------------------------------------------------------------------------
+
+class Director:
+    __builder = None
+
+    def setBuilder(self, builder):
+        self.__builder = builder
+    
+    def getBook(self):
+        book = Book()
+
+        format = self.__builder.getFormat()
+        book.setFormat(format)
+
+        while True:
+            page = self.__builder.getPage()
+            if(page == 'quni'):
+                break
+            book.setPage(page)
+        return book
+    
+class Book:
+    def __init__(self):
+        self.__format = None
+        self.__pages = list()
+
+    def setFormat(self, format):
+        self.__format = format
+
+    def setPage(self, page):
+        self.__pages.append(page)
+
+class Builder:
+    def getFormat(self): pass
+    def getPage(self): pass
+    
+class CreateBook(Builder):
+    def getFormat(self):
+        format = input('Введіть формат: ')
+        return format
+    
+    def getPage(self):
+        page = input('Пишіть: ')
+        return page
+    
+
+director = Director()
 
 
-def delay_print(s):
-    for c in s:
-        print(c, end="")
-        time.sleep(random.random() * 0.01)
-    print()
+#-----------------------------------------------------------------------------------------
+#                                     Abstract Factory
+#-----------------------------------------------------------------------------------------
 
+class CombinedBook: 
+    def __init__(self, part1, part2):
+        self.description = part1
+        self.book = part2
 
-character_name = input("Вітаю! Введіть ім'я вашого персонажа: ")
-delay_print(
-    "Вітаю, "
-    + character_name
-    + "! Тобі належить вибрати свій шлях у невеликій грі, в результаті якої ти дізнаєшся який сік компанії Січко™ тобі підходить найбільше!"
-)
+class AbstractLibrary:
+    def sell(self):
+        raise Exception("no books to sell")
+class ScienceBook(AbstractLibrary):
+    def usedLitList(self, used_list):
+        self.used_list = used_list
+    def add_glossarium(self, glossarium ):
+        self.glossarium = glossarium
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return (f"{self.name}")
+class NovelBook(AbstractLibrary):
+    def character_list(self, list):
+        self.char_list = list
+    def brief_description(self, desc):
+        self.brief_desc = desc
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return (f"{self.name}")
+class ManualBook(AbstractLibrary):
+    def add_image(self, image):
+        self.image = image
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return (f"{self.name}")
+    
+class AbstractLibraryFactory:
+    def create(self):
+        raise Exception("this is not book class")
+class ScienceBookFactory(AbstractLibraryFactory):
+    def create(self):
+        book = ScienceBook('Science Book')
+        book.usedLitList(input('Введіть список викорастної літератури: '))
+        book.add_glossarium(input('Введіть глосаріум: '))
+        director.setBuilder(CreateBook())
+        scienceBook = director.getBook()
+        fullBook = CombinedBook(book, scienceBook)
+        return fullBook
+class NovelBookFactory(AbstractLibraryFactory):
+    def create(self):
+        book = NovelBook('Novel Book')
+        book.character_list(input('Введіть список персонажів: '))
+        book.brief_description(input('Введіть короткий опис: '))
+        director.setBuilder(CreateBook())
+        scienceBook = director.getBook()
+        fullBook = CombinedBook(book, scienceBook)
+        return fullBook     
+class ManualBookFactory(AbstractLibraryFactory):
+    def create(self):
+        book = ManualBook('Manual Book')
+        book.add_image(input('Введіть ссилку на картинку: '))
+        director.setBuilder(CreateBook())
+        scienceBook = director.getBook()
+        fullBook = CombinedBook(book, scienceBook)
+        return fullBook
+    
+def create_book(bookFactory: AbstractLibraryFactory):
+    return bookFactory.create()
 
-answers_array = []
-
-quest_index = 0
-
-delay_print(
-    "Вітаю на першому рівні! До тебе підходить невідоме створіння, схоже на гнома, і каже:\n'Привіт, незнайомцю. Якщо ти хочеш пройти, то тобі потрібно відгадати загадку. Якщо ти не хочеш, то я тебе вб'ю.'"
-)
-
-print(
-    "Варіанти відповідей:\n1) 'Так, я хочу відгадати загадку.'\n2) 'Ні, я не хочу відгадувати загадку. (почати пізділовку)'"
-)
-
-answer = input("Введіть відповідь: ")
-answers_array.append(answer)
-
-match answer:
-    case "1":
-        delay_print(
-            "Гном: 'Добре, тоді почнемо. Якщо ти відгадаєш, то я тобі дам піти.'"
-        )
-    case "2":
-        delay_print("Гном: 'Ти вирішив битися? Тоді почнемо.'")
-    case _:
-        delay_print(
-            "Гном: 'Я не розумію тебе. Якщо ти не зможеш відповісти, то я тебе вб'ю.'"
-        )
-
-
-quest_index = 1
-previous_answer = answers_array[quest_index - 1]
-
-quest_answer = 0
-next_answer = 0
-
-match previous_answer:
-    case "1":
-        delay_print(
-            "Гном: 'У мами Івана було три сини. Першого звали Петро, другого – Василь. Як звали третього сина?'"
-        )
-        print("Варіанти відповідей:\n1) 'Іван'\n2) 'Петро'\n3) 'Василь'")
-        quest_answer = input("Введіть відповідь: ")
-        answer = 1
-    case "2":
-        print("*Гном розмахується кулаком і ударяє вас по коліну.*")
-        delay_print("Гном: 'Н-Н-Н-А-А-А!!!'")
-        print(
-            "*Ви відчуваєте сильний біль в коліні але він не надто сильний*\n1. *пнути гнома з усієї сили ногою*\n2. Будь ласка!!! Не бий! Я відповім на твою загадку!!!"
-        )
-        next_answer = "2"
-        answer = input("Введіть відповідь: ")
-    case _:
-        delay_print("Гном: 'Шо ти там бормочеш??!!'")
-
-quest_index = 2
-answers_array.append(answer)
-previous_answer_2 = answers_array[quest_index - 1]
-
-print(previous_answer_2)
-print(quest_answer)
-
-match previous_answer_2:
-    case "1":
-        if quest_answer == "1":
-            delay_print(
-            "Гном: 'Ти красавец, єжі!"
-            )
-            quest_answer = input("Введіть відповідь на комплімент: ")
-        else:
-            delay_print(
-            "Гном: 'Ти даун, неправильно!"
-            )
-            answer = input("Введіть відповідь на глузування: ")
-            
-    case "2":
-        print("*Гном розмахується кулаком і ударяє вас по коліну.*")
-        delay_print("Гном: 'Н-Н-Н-А-А-А!!!'")
-        print(
-            "*Ви відчуваєте сильний біль в коліні але він не надто сильний*\n1. *пнути гнома з усієї сили ногою*\n2. Будь ласка!!! Не бий! Я дам тобі піздлюей!!!"
-        )
-        answer = input("Введіть відповідь: ")
-        delay_print("Пізда тобі мразота!")
-        print("Ви пізданули гнома зі всієї сили ногою і в нього скрутився м'яз на ляшці.")
-        print('Гном корчитсья від болі, а ви смієітеесь')
-        delay_print("ХА-ХА-ХА-ХА-ХА ЛЕГЧАЙШАЯ ДЛЯ КОГО ДЛЯ ВЕЛИЧАЙШЕГО")
-    case _:
-        delay_print("Гном: 'Шо ти там бормочеш чмо??!!'")
+scienceBook = create_book(ScienceBookFactory())
